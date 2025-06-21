4e0638b377e5eb6a6e86924e27d9538b3e503668
Joe <jgacheru504@gmail.com>
2025-06-21 12:55:36 +0300

    added dropdown menu and some other changes

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,14 +1,22 @@
 import "./About.css"
 import { FaRegArrowAltCircleDown, FaReact, FaHtml5, FaCss3, FaGithub, FaGit, FaFigma } from "react-icons/fa"
 import { SiJavascript } from "react-icons/si"
+import my_photo from "../images/my_pic.png"
 
 export default function About(){
     return(
         <section className="about">
             <header>About Me</header>
-            <img src="" alt="a photo of me" />
-            <p>I'm a passionate front-end developer with a strong focus on creating clean, responsive, and user-friendly websites. I specialize in building with React, translating designs into functional, mobile-first interfaces. With experience in version control (Git/Github) and deployment platforms like Netlify, I enjoy turning ideas into real-world digital experiences. I'm continuously learning and refining my skills to deliver fast, accessible, and modern web applications</p>
-            <button className="cvBtn"><FaRegArrowAltCircleDown className="arrowDown" /> Download CV</button>
+            <article className="aboutAll">
+                <div className="imgContainer">
+                    <img className="myPhoto" src={my_photo} alt="a photo of me" />
+                </div>
+                <div className="aboutText">
+                    <p>I'm a passionate front-end developer with a strong focus on creating clean, responsive, and user-friendly websites. I specialize in building with React, translating designs into functional, mobile-first interfaces. With experience in version control (Git/Github) and deployment platforms like Netlify, I enjoy turning ideas into real-world digital experiences. I'm continuously learning and refining my skills to deliver fast, accessible, and modern web applications</p>
+                    <button className="cvBtn"><FaRegArrowAltCircleDown className="arrowDown" /> Download CV</button>
+                </div>
+            </article>
+            
             <article className="skills">
                 <figure>
                     <FaReact className="skillIcon"/>
--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,22 +1,45 @@
 import { FaBars } from 'react-icons/fa'
 import './Nav.css'
+import { useState } from 'react'
+import { SiX } from 'react-icons/si'
+
 export default function Nav(){
+
+    const [menuPressed, setMenuPressed] = useState(false)
+    function menuClick(){
+        setMenuPressed (prev=>(!prev))
+    }
+    function goToContact(){
+        console.log('contact btn pressed');
+        
+    }
     return(
         <div className='navContainer'>
             <nav className='navbarMobile'>
-                <FaBars size='20px' className='menuIcon'/>
-                <span>Logo</span>
-                <button>Contact Me</button>
+                {menuPressed ? 
+                <SiX className="menuIcon" onClick={menuClick} /> :
+                <FaBars size='20px' className='menuIcon' onClick={menuClick}/>
+                }
+                <span className='logo'>Logo</span>
+                <button className='navBtn' onClick={goToContact}>Contact Me</button>
             </nav>
+            {menuPressed ? 
+            <div className='dropMenuList'>
+                <div>Home</div>
+                <div>Services</div>
+                <div>About</div>
+                <div>Connect</div>
+            </div> : null}
+            
             <nav className='navbarDesktop'>
-                <span>Logo</span>
+                <span className='logo'>Logo</span>
                 <div className='navLinks'>
                     <div>Home</div>
                     <div>Services</div>
                     <div>About Me</div>
                     <div>Connect</div>
                 </div>
-                <button>Contact Me</button>
+                <button className='navBtn' onClick={goToContact}>Contact Me</button>
             </nav>
         </div>
     )
