05df71998e981d019afb6722f3b30baedcfe387d
Joe <jgacheru504@gmail.com>
2025-09-10 00:18:41 +0300

    added project 1 and 2 images and descriptions

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,9 +1,19 @@
-
-export default function ProjectCard(props){
+import { ArrowDownRight, Home } from "lucide-react"
+import "./ProjectCard.css"
+export default function ProjectCard({description, projectCaption, projectImg, link}){
     return(
         <div className="projectCard">
-            <img src={props.projectImg} />
-            <span>{props.projectCaption}</span>        
+            
+            <img src={projectImg} />
+            <span>
+                {projectCaption} 
+                <button
+                    onClick={()=> {window.open(link, '_blank')}}
+                >
+                    <ArrowDownRight />
+                </button>
+            </span> 
+            <p>{description}</p>       
         </div>
     )
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,26 +1,34 @@
 import ProjectCard from "./ProjectCard"
 import "./Projects.css"
+import finImg from "../images/ProjectFin.png"
+import recImg from "../images/ProjectRecipe.png"
+
 
 export default function Projects(){
     return(
         <section className="projects">
             <header>Projects</header>
-            <ProjectCard 
-                projectImg=''
-                projectCaption="Project 1"
+            <div>
+                
+            <ProjectCard
+                link= "https://gi-joe254.github.io/finance-tracker/"
+                description= 'A personal finance and budgeting app that allows users to create acounts, save and edit transactions, calculate and save budgets, and compare transactions data.' 
+                projectImg={finImg}
+                projectCaption="Finance Tracker"
             />
-            <ProjectCard 
-                projectImg=''
-                projectCaption="Project 2"
+            <ProjectCard
+                link= "https://gi-joe254.github.io/recipe-generator/"
+                description= 'An online recipe generator that allows users to input ingredients and submit to get custom recipes.' 
+                projectImg={recImg}
+                projectCaption="Recipe Generator"
             />
-            <ProjectCard 
-                projectImg=''
-                projectCaption="Project 3"
+            <ProjectCard
+                link= "https://github.com/Gi-Joe254"
+                description= 'A business website for an electronics sales and repair shop.' 
+                projectImg='null'
+                projectCaption="Electronics Shop "
             />
-            <ProjectCard 
-                projectImg=''
-                projectCaption="Project 3"
-            />
+            </div>
         </section>
     )
 }
